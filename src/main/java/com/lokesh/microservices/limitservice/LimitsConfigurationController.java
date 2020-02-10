8951e41018b58278d6297faf3565cc600361731f
Lokesh <lokesh16191@gmail.com>
2020-02-10 07:41:42 +0530

    ok

--- src/main/java/com/lokesh/microservices/limitservice/LimitsConfigurationController.java
+++ src/main/java/com/lokesh/microservices/limitservice/LimitsConfigurationController.java
@@ -4,13 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
 	@Autowired
 	private Configuration configuration;
 	@GetMapping("/limits")
+
 	public LimitConfiguration retrivelLimitsFromConfigurations(){
 		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retriveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(9,999);
 	}
 	
 }
